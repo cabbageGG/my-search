e4bcf42fdc50c67e4f28bc844082b3252516a2b7
yangjin <13246856469@163.com>
2017-11-24 09:42:54 +0800

    add suggestview process

--- lcv_search/search/views.py
+++ lcv_search/search/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from search.models import ArticleType
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -9,7 +12,21 @@ class IndexView(View):
 
 class SuggestView(View):
     def get(self, request):
-        return render(request, "index.html")
+        key_words = request.GET.get('s', '')
+        re_datas = []
+        if key_words:
+            s = ArticleType.search()
+            s = s.suggest('my_suggest', key_words, completion={
+                "field":"suggest", "fuzzy":{
+                    "fuzziness":2
+                },
+                "size":10
+            })
+            suggestions = s.execute_suggest()
+            for match in suggestions.my_suggest[0].options:
+                source = match._source
+                re_datas.append(source["title"])
+        return HttpResponse(json.dumps(re_datas), content_type="application/json")
 
 class SearchView(View):
     def get(self, request):
